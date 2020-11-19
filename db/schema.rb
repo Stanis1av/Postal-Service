496b4750f914d37bf9eab71ce1ca6cf199d6f75c
Stanis1av <krasavin.stanislaus@gmail.com>
2020-11-19 14:06:48 +0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_170833) do
+ActiveRecord::Schema.define(version: 2020_11_19_104615) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 2020_11_18_170833) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
+    t.boolean "superadmin_role", default: false
+    t.boolean "admin_role", default: false
+    t.boolean "employee_role", default: false
+    t.boolean "user_role", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
