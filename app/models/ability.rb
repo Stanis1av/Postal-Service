9a24ce15874069cef22560906ceb332fbaa1b17d
Stanis1av <krasavin.stanislaus@gmail.com>
2020-11-19 14:13:58 +0300

    Allocation of rights for roles

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,7 +6,20 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-    #   user ||= User.new # guest user (not logged in)
+      user ||= User.new # guest user (not logged in)
+
+        if user.superadmin_role?
+          can :manage, :all
+        end
+
+        if user.admin_role?
+          can :manage, :all
+        end
+
+        if user.employee_role?
+          can :manage, User
+        end
+
     #   if user.admin?
     #     can :manage, :all
     #   else
