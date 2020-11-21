d8a443772468cf109bcda40dc9cb6f3e6919f3f6
Stanis1av <krasavin.stanislaus@gmail.com>
2020-11-22 00:36:45 +0300

    Add output information about user to profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,5 @@
 class ProfileController < ApplicationController
   def index
-    @profile = Profile.find(params[:id]).user_id
+    @profile = Profile.find(current_user.id)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :profile
+  before_create :build_profile
 end
