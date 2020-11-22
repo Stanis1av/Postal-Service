8122265b41da7790202bda7223ee7d7414773267
Stanis1av <krasavin.stanislaus@gmail.com>
2020-11-22 18:37:37 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  get 'profile/index'
+  # get 'profiles/show'
+  # get 'profile/index'
+
   devise_for :users
+
   get 'mailing/index'
 
   root to: 'mailing#index'
+
+  resource :profile, only: [:show, :edit, :update], path_names: { :show => '' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
