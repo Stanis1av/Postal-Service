804d89b36be21ae205516d60e94841b5a4738f45
Stanis1av <krasavin.stanislaus@gmail.com>
2020-11-22 18:36:37 +0300

    Update Profiles controller and him views and also with another things

--- app/controllers/profile_controller.rb
+++
@@ -1,5 +0,0 @@
-class ProfileController < ApplicationController
-  def index
-    @profile = Profile.find(current_user.id)
-  end
-end
---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,23 @@
+class ProfilesController < ApplicationController
+  def show
+    @profile = Profile.find(current_user.id)
+  end
+
+  def edit
+    @profile = Profile.find(current_user.id)
+  end
+
+  def update
+    @profile = Profile.find(current_user.id)
+
+    if @profile.update(profile_params)
+      redirect_to profile_path
+    else
+      render action: 'edit'
+    end
+  end
+
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :home_address)
+  end
+end
