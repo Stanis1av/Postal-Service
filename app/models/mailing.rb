cac6e58d79116e8633bca62e66f981bc2b55f709
Stanis1av <krasavin.stanislaus@gmail.com>
2020-11-25 19:49:26 +0300

    Add connections one to many for user and mailings

--- app/models/mailing.rb
+++ app/models/mailing.rb
@@ -1,2 +1,3 @@
 class Mailing < ApplicationRecord
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :profile
+  has_many :mailings
+
   before_create :build_profile
 end
