10ebfdcc60cbb007543e9a5c9eaac597207c0b85
Stanis1av <krasavin.stanislaus@gmail.com>
2020-11-26 12:50:11 +0300

    Update function for create track number

--- app/controllers/mailings_controller.rb
+++ app/controllers/mailings_controller.rb
@@ -12,8 +12,16 @@ class MailingsController < ApplicationController
   end
 
   def create
-    @mailing = Mailing.new(mailing_params)
+    @mailing = Mailing.new#(mailing_params)
     @mailing.user_id = current_user.id
+
+    @departure_point = '344000'
+    @place_of_arrival = '347935'
+
+    create_track
+
+    @mailing.track_number = @new_track_number
+
     if @mailing.save
       redirect_to @mailing
     else
@@ -22,9 +30,19 @@ class MailingsController < ApplicationController
     end
   end
 
-  private
+  def create_track
+    t = Time.now
+    data = t.strftime("%d%m%Y")
 
-  def mailing_params
-    params.require(:mailing).permit(:track_number)
+    nof = current_user.id.to_s
+
+    @new_track_number = 'RR' + data + nof + @departure_point + @place_of_arrival
   end
+
+  # private
+
+  # def mailing_params
+  #   params.require(:mailing).permit(:track_number)
+  # end
+
 end
