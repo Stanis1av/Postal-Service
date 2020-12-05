306d455e941eb823961a2f3ba040981d143663fa
Stanis1av <krasavin.stanislaus@gmail.com>
2020-12-05 15:20:32 +0300

    Moving from the database SQLite3 to Postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2020_11_22_173349) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "mailings", force: :cascade do |t|
     t.string "track_number"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_mailings_on_user_id"
@@ -24,7 +27,7 @@ ActiveRecord::Schema.define(version: 2020_11_22_173349) do
     t.string "first_name"
     t.string "last_name"
     t.string "home_address"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
