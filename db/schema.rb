015ed9b4cf324bd50a241f33efcd845f8f2a08a4
Stanis1av <krasavin.stanislaus@gmail.com>
2020-12-05 21:07:52 +0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_22_173349) do
+ActiveRecord::Schema.define(version: 2020_12_05_175557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2020_11_22_173349) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_mailings_on_user_id"
+  end
+
+  create_table "postal_codes", force: :cascade do |t|
+    t.bigint "postcode"
+    t.string "city"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "profiles", force: :cascade do |t|
