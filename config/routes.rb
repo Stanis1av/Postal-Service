03a8d68b123afca7e7d706cfa4c8392524018083
Stanis1av <krasavin.stanislaus@gmail.com>
2020-12-07 22:31:20 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,11 @@ Rails.application.routes.draw do
   root to: 'mailings#index'
 
   resource :profile, only: [:show, :edit, :update], path_names: { :show => '' }
+
+  resource :dashboard, only: [:show, :new, :create, :edit, :update], path_names: { :show => '' }
+
+  resource :postal_code, only: [:create] #, path_names: { :create => '' }
   resources :mailings
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
